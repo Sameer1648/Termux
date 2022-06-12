e3709923aae3229176808a2c0be24112a29b6f2b
Sameer <107326224+Sameer1648@users.noreply.github.com>
2022-06-12 19:59:35 +0900

    Create Termux.sh
    apt update
    apt upgrade
    apt install python
    apt install python2
    apt install git
    pip2 install requests
    pip2 install mechanize
    https://github.com/Sameer1648/Termux.git
    cd Termux
    sh Termux.sh

---
+++ Termux.sh
@@ -0,0 +1,73 @@
+
+echo ""
+echo ""
+echo ""
+echo "The script is  made by Sameer King"
+echo ""
+echo ""
+echo ""
+echo "subscribe  our channel"
+echo ""
+echo ""
+echo ""
+echo "Installtion process is started" 
+echo ""
+echo ""
+echo ""
+
+pkg update -y                    
+pkg install python -y           
+pkg install python2 -y
+pkg install ruby -y 
+pkg install git -y
+pkg install screenfetch -y
+pkg install cowsay -y
+pkg install toilet -y
+pkg install figlet -y
+pkg install php -y 
+pkg install perl -y
+pkg install nmap -y
+pkg install openssh -y
+pkg install bash -y
+pkg install clang -y
+pkg install jq -y
+pkg install nano -y
+pkg install curl -y
+pkg install tar -y
+pkg install zip -y
+pkg install fish -y
+pkg install unzip -y
+pkg install tor -y 
+pkg install sudo -y
+pkg install wget -y
+pkg install tor -y
+pkg install w3m -y
+pkg install cmatrix -y
+pkg install wcalc -y 
+pkg install openssl -y
+pkg install bmon -y
+pkg upgrade -y
+echo ""
+echo ""
+echo ""
+echo ""
+
+echo "All basic packages and tools is installed"
+echo ""
+echo ""
+echo "
+$$$$$$\                                                        
+$$  __$$\                                                       
+$$ /  \__| $$$$$$\  $$$$$$\$$$$\   $$$$$$\   $$$$$$\   $$$$$$\  
+\$$$$$$\   \____$$\ $$  _$$  _$$\ $$  __$$\ $$  __$$\ $$  __$$\ 
+ \____$$\  $$$$$$$ |$$ / $$ / $$ |$$$$$$$$ |$$$$$$$$ |$$ |  \__|
+$$\   $$ |$$  __$$ |$$ | $$ | $$ |$$   ____|$$   ____|$$ |      
+\$$$$$$  |\$$$$$$$ |$$ | $$ | $$ |\$$$$$$$\ \$$$$$$$\ $$ |      
+ \______/  \_______|\__| \__| \__| \_______| \_______|\__|      
+                                                                
+echo " telegram chanel  : https://t.me/Sameerking1648
+youtube chanel : Sameer King 
+github ; https://github.com/Sameer1648"
+echo ""
+echo " this sceipt craeted by Sameer King 👌😌❤🙋‍♂️   Subscribe our Youtube channel and press Bell icon"
+exit
